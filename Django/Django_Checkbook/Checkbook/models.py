6a48dceb81c2cc6bd74e89a6502cb28a0544c33d
Ian Marshall <96323360+IMarshall@users.noreply.github.com>
2022-03-30 22:27:22 -0600

    Added Checkbook Project

---
+++ Django/Django_Checkbook/Checkbook/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+class Account(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    initial_deposit = models.DecimalField(max_digits=15, decimal_places=2)
+
+    Accounts = models.Manager()
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+
+TransactionTypes = [('Deposit', 'Deposit'), ('Withdrawal', 'Withdrawal')]
+
+class Transaction(models.Model):
+    date = models.DateField()
+    type = models.CharField(max_length=10, choices=TransactionTypes)
+    amount = models.DecimalField(max_digits=15, decimal_places=2)
+    description = models.CharField(max_length=100)
+    account = models.ForeignKey(Account, models.CASCADE)
+
+    Transactions = models.Manager()
